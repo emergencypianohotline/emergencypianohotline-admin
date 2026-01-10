7803342c455b83f2d2a648a995efaecebad891e3
Josh Cohen <hello@dntwig.com>
2026-01-10 11:32:25 +0100

    Update to correct Supabase anon key

--- assets/js/config.js
+++ assets/js/config.js
@@ -7,7 +7,7 @@ export const DEBUG = true;
 
 // Supabase configuration
 export const SUPABASE_URL = 'https://iqvntlifrvkdqrrcxwsf.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlxdm50bGlmcnZrZHFycmN4d3NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5ODI4ODgsImV4cCI6MjA0ODU1ODg4OH0.VoCWNjqBN6pHVIEcdYqUjPvG0gaCxC6jz_bz8DLwPHA';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlxdm50bGlmcnZrZHFycmN4d3NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNzc5NzIsImV4cCI6MjA3NDc1Mzk3Mn0.wbCHI_BFXkjI0foLKQBVZShSBh8fM3TvX545xND1hbc';
 
 // Debug logger
 export const debug = {
